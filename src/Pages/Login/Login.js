627e0efe2993abcebc255db7cbd1ae3a16583bba
Mahdi <mohamadmahdiy@gmail.com>
2019-06-13 11:58:58 +0430

    Make connection between login page and back end

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,27 +4,53 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Checkbox from '@material-ui/core/Checkbox';
-import Signup from '../Signup/Signup';
-
 
 export default class Login extends React.Component{
     state = {
         checkedA: true,
+        username:"",
+        password:"",
+        
       };
       handleChange = name => event => {
         this.setState({ [name]: event.target.checked });
       };
+      handleChanger(e) {
+        this.setState({ [e.target.name]: e.target.value });
+      }
+      handleSubmit= e => {
+        
+        fetch('http://127.0.0.1:8000/get_token/', {
+            mode:"cors",
+			method: 'POST',
+			body: JSON.stringify({
+				username: this.state.username,
+				password: this.state.password
+			}),
+			headers: {
+				"Content-type": "application/json; charset=UTF-8"
+			}
+		}).then(response => {
+				return response.json()
+			}).then(json => {
+                console.log(json)
+            });
+
+        };
+        
     
     render(){
+        // const {responsee , data , log} = this.state
         return(
+
             <div className= "login_form">
             <Paper elevation={4} className="login_paper" >      
                 <h1 ><center>Login</center></h1>
                 <div className="fields">
-                 <TextField id="outlined-email-input" fullWidth className="usertext" label="User Name" type="email" name="email" autoComplete="email" margin="normal"/>
+                <TextField onChange={this.handleChanger.bind(this)} value={this.state.username} id="outlined-email-input" fullWidth className="usertext" label="userName" type="email" name="username" autoComplete="email" margin="normal"/>
                 </div>
                 <div className="fields">
-                    <TextField id="outlined-password-input" fullWidth className="passtext" label="Password"type="password"autoComplete="current-password"margin="normal"/>
+                <TextField onChange={this.handleChanger.bind(this)} value={this.state.password} id="outlined-password-input" fullWidth className="passtext"  label="password" name='password' type="password"autoComplete="current-password"margin="normal"/>
                 </div>
                 <div className = "check_box">
                     <Checkbox
@@ -35,11 +61,11 @@ export default class Login extends React.Component{
                     />remember me
                 </div>
                 <div className='btn-submit'>
-                    <Button variant="contained" color = "primary" fullWidth onClick = {Signup}>
+                    <Button variant="contained" color = "primary" fullWidth onClick={this.handleSubmit}>
                         Log in
                     </Button>
                 </div>      
-            <h3 className = "forgot_password"><a href = "www.google.com">forgot your password?</a> </h3>  
+            <h3 className = "forgot_password">forgot your password? </h3>  
             <h5 className = "signup">not a member ? <a href = "/Signup">sign up now</a> </h5>    
             </Paper>
             </div>
@@ -47,4 +73,3 @@ export default class Login extends React.Component{
 
     }
 }
-
