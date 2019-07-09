459b3fc52c5cb71332d99540e42dd926ce910da0
ghazale <gh_mhdi@outlook.com>
2019-07-09 18:04:50 +0430

    Update additional file

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Login from './Pages/Login/Login'
 import Signup from './Pages/Signup/Signup'
 import Homepage from './Pages/Home/Home'
 import Docprofile from './Pages/DoctorProfile/DoctorProfile'
-import Searched from './Pages/Home/Searched'
-import { BrowserRouter as Router, Route} from "react-router-dom";
-
+import PatientProfile from './Pages/PatientProfile/PatientProfile'
+import Doc from './Pages/DoctorPage/Doctor'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+ 
 const routing = (
     <Router>
-        <div>
-            <Route path="/Login" component={Login} />
-            <Route path="/Signup" component={Signup} />
-            <Route path="/Homepage" component={Homepage} />
-            <Route path="/DoctorProfile" component={Docprofile} />
-            <Route path="/Searched" component={Searched} />
-        </div>
+      <div>
+        <Route path="/Login" component={Login} />
+        <Route path="/Signup" component={Signup} />
+        <Route path="/Homepage" component={Homepage} />
+        <Route path="/DoctorProfile" component={Docprofile} />
+        <Route path="/PatientProfile" component={PatientProfile} />
+        <Route path="/Doctor" component={Doc} />
+      </div>
     </Router>
 )
 export default routing
