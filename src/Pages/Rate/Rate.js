42a1c66ceaf0699c6c1edf739a855650f68f98f6
ghazale <gh_mhdi@outlook.com>
2019-07-09 17:58:39 +0430

    Add rate

---
+++ src/Pages/Rate/Rate.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import StarRatingComponent from 'react-star-rating-component';
+
+export default class Rate extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <StarRatingComponent
+          name="rate1"
+          starCount={5}
+          editing={false}
+          value={this.props.Rate}
+        />
+      </div>
+    );
+  }
+}
