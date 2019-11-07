eb539a48401902f87a1ad78526104faff9bf4cc6
Mahdi <mohamadmahdiy@gmail.com>
2019-11-07 23:12:55 +0330

    Add medical history to patient profile page

--- src/Pages/PatientProfile/PatientProfile.js
+++ src/Pages/PatientProfile/PatientProfile.js
@@ -4,6 +4,7 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import MenuAppBar from '../Home/NavBar';
 import ViewInfo from "./AppointmentTimeInfo";
+import Medicalhistory from "./MedicHistory";
 import ViewAndEditPatientInformation from "./PatientInfo";
 import { connect } from "react-redux";
 import * as patientProfile_api from "../../Redux/PatientProfile/PatientProfile_action";
@@ -14,7 +15,7 @@ class PatientProfile extends React.Component {
         super(props);
         this.state = {
             clickOnClinic: false,
-            ReservatiomTime: [],
+            hsitory: false,
             info: false,
             reserve: true,
         };
@@ -26,53 +27,73 @@ class PatientProfile extends React.Component {
 
     }
 
+    handlehistory = (e) => {
+        this.setState({
+            info: false,
+            reserve: false,
+            hsitory:true,
+        })
+    }
     handleinfo = (e) => {
         this.setState({
             info: true,
             reserve: false,
+            hsitory:false
         })
     }
 
     handlereserve = (e) => {
         this.setState({
             reserve: true,
-            info: false
-
+            info: false,
+            hsitory:false
         })
     }
 
 
     render() {
-        console.log(this.props.Patient[0])
+        // console.log(this.props.Patient[0])
         return (
             <div>
                 <MenuAppBar />
-            <div>
+                <div>
                     <Grid container spacing={24}>
                         <Grid item sm={9} style={{ paddingTop: "2%", paddingLeft: "5%", paddingRight: "5%" }}>
-                            {this.state.reserve  ? (
-                                <div>           
-                                    {typeof(this.props.Patient[0])!= "undefined" ? 
+                            {this.state.reserve ? (
+                                <div>
+                                    {typeof (this.props.Patient[0]) != "undefined" ?
                                         <div>
                                             {this.props.Patient[0].patient_appointment_times.map(appointment => <ViewInfo Appointment={appointment} />)}
                                         </div>
-                                    :console.log("khodeti")}             
+                                        : console.log("khodeti")}
                                 </div>
                             )
-                                :this.state.info ?
+                                : this.state.info ?
                                     <Paper elevation={5} style={{ 'marginTop': "3%", 'paddingRight': "4%", 'paddingLeft': "1%", opacity: "0.9" }}>
-                                        <ViewAndEditPatientInformation/>
+                                        <ViewAndEditPatientInformation />
                                     </Paper>
-                                    : "welcome ...   "
+                                    : this.state.hsitory ? (
+                                        <div>
+                                            {typeof (this.props.Patient[0]) != "undefined" ?
+                                                <div>
+                                                    <Medicalhistory />
+                                                </div>
+                                                : console.log("khodeti")}
+                                        </div>
+                                    ) :
+                                        "welcome ...   "
                             }
                         </Grid>
                         <Grid item sm={3} style={{ paddingTop: "2%", paddingRight: "2%" }}>
 
-                            <Button variant="outlined" fullWidth onClick={this.handleinfo} style={{ paddingBottom: "0", paddingTop: "0",marginBottom:"3%", borderRadius: '12%' }}>
+                            <Button variant="outlined" fullWidth onClick={this.handleinfo} style={{ paddingBottom: "0", paddingTop: "0", marginBottom: "3%", borderRadius: '12%' }}>
                                 <h4>مشخصات</h4>
                             </Button>
-                            <Button variant="outlined" fullWidth onClick={this.handlereserve} style={{ paddingBottom: "0", paddingTop: "0", borderRadius: '12%' }}>
+                            <Button variant="outlined" fullWidth onClick={this.handlereserve} style={{ paddingBottom: "0", paddingTop: "0", borderRadius: '12%', marginBottom: "3%" }}>
                                 <h4>وقت های رزرو شده</h4>
+                            </Button>
+                            <Button variant="outlined" fullWidth onClick={this.handlehistory} style={{ paddingBottom: "0", paddingTop: "0", borderRadius: '12%' , marginBottom: "3%" }}>
+                                <h4>سوابق پزشکی</h4>
                             </Button>
                         </Grid>
                     </Grid>
@@ -82,13 +103,13 @@ class PatientProfile extends React.Component {
         )
     }
 }
-    const mapStateToProps = state => ({
-        ...state,
-        Patient:state.PatientProfile_reducer.patientProfile_load_result
-    });
-    
-    const mapDispatchToProps = dispatch => ({
-        PatientProfile_load: () => dispatch(patientProfile_api.PatientProfile_load())
-    });
+const mapStateToProps = state => ({
+    ...state,
+    Patient: state.PatientProfile_reducer.patientProfile_load_result
+});
+
+const mapDispatchToProps = dispatch => ({
+    PatientProfile_load: () => dispatch(patientProfile_api.PatientProfile_load())
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(PatientProfile)
