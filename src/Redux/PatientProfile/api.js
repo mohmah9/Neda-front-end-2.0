92fd755ebec23d68218859c9fd0bb6c23c78ef12
Mahdi <mohamadmahdiy@gmail.com>
2019-11-07 22:58:56 +0330

    Complete patient profile reducer and api for medical history

--- src/Redux/PatientProfile/api.js
+++ src/Redux/PatientProfile/api.js
@@ -41,7 +41,7 @@ class api{
           }),
           headers: {
             "Content-type": "application/json; charset=UTF-8",
-            "Authorization": "Token " + localStorage.getItem('token')
+            "Authorization": "Token " + sessionStorage.getItem('token')
           }
         })
       
@@ -106,6 +106,25 @@ class api{
     x = await x.json()
   
     if (typeof (x.address) == "undefined") {
+        return false
+    } else {
+        return x;
+    }
+  };
+  
+  static loadMedical_history = async (patient_id) => {
+    
+    let x = await fetch("http://172.17.3.103:8000/medical_histories/?patient="+patient_id, {
+      mode: "cors",
+      method: 'GET',
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+    })
+
+    x = await x.json()
+  
+    if (typeof (x) == "undefined") {
         return false
     } else {
         return x;
