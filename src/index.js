3c50ee3fed6550750963b37d786d7b624f7a64e7
Mahdi <mohamadmahdiy@gmail.com>
2019-11-08 23:23:27 +0330

    Add redux to react index and make store available to components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import routing from './App.js';
+import Routing from "./App"
 import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import configureStore from "./Redux/store";
 
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={configureStore()}>
+        <Routing />
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
