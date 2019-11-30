7dedb71886d0fef2ee59069275ad659aadac134c
mohamad <mohamadmahdiy@hotmail.com>
2019-11-30 14:56:26 +0000

    Revert "Merged PR 34: Add redux to react index and make store available to components
    Add redux to react index and make store available to components"

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Routing from "./App"
+import routing from './App.js';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from "react-redux";
-import configureStore from "./Redux/store";
 
-ReactDOM.render(
-    <Provider store={configureStore()}>
-        <Routing />
-    </Provider>
-    , document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
