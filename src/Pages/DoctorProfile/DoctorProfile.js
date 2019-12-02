773c753104df5f07e780ff49125aecf6c1260509
ghazale <gh_mhdi@outlook.com>
2019-12-02 18:09:59 +0330

    Add map and chat ui to doctor profile page

--- src/Pages/DoctorProfile/DoctorProfile.js
+++ src/Pages/DoctorProfile/DoctorProfile.js
@@ -8,16 +8,24 @@ import MenuItem from '@material-ui/core/MenuItem';
 import MenuAppBar from '../Home/NavBar'
 import { connect } from "react-redux";
 import Rate from '../Rate/Rate'
+import Chat from '../Chat/Chat'
+import App from '../Map/Map'
+import { Button } from '@material-ui/core';
+import { BrowserRouter as Router, Route, Link, Redirect, withRouter } from "react-router-dom";
 
 class DoctorProfile extends React.Component {
 
     state = {
         clickOnClinic: false,
         selectedClinic: [],
-        clinics: []
+        clinics: [], 
+        Map : false
 
     };
-    
+
+    handleClickOnMap = () => {
+        this.setState({ Map: true})
+      };
 
     handleclick = async (clinic) => {
         await this.setState({ selectedClinic: null, clickOnClinic: false })
@@ -31,9 +39,10 @@ class DoctorProfile extends React.Component {
     }
 
     render() {
+        if (this.state.Map) return <Redirect to={{ pathname: '/Map' }} />
         return (
             <div>
-             <MenuAppBar />
+                <MenuAppBar />
                 <Grid container spacing={24}>
 
                     <Grid item xs={12}>
@@ -41,12 +50,12 @@ class DoctorProfile extends React.Component {
 
                     </Grid>
                     <Grid item sm={5} style={{ paddingTop: "2%", paddingLeft: "5%", paddingRight: "5%" }}>
-                        <Paper style={{ opacity: "0.9" ,  background: "linear-gradient(to bottom, #83a4d4, #b6fbff)"}}>
+                        <Paper style={{ opacity: "0.9", background: "linear-gradient(to bottom, #83a4d4, #b6fbff)" }}>
                             {this.state.clickOnClinic ? <Calender clinic={this.state.selectedClinic} /> : null}
                         </Paper>
                     </Grid>
                     <Grid item sm={2} style={{ paddingTop: "2%", paddingRight: "5%" }}>
-                        <Paper style={{ opacity: "0.9" , background:" linear-gradient(to left, #1c92d2, #f2fcfe)"}}>
+                        <Paper style={{ opacity: "0.9", background: " linear-gradient(to left, #1c92d2, #f2fcfe)" }}>
                             <MenuList>
                                 <p style={{ 'paddingRight': "5%", 'textAlign': "right" }}>مطب ها و بیمارستان ها</p>
                                 {this.props.doctor.doctor_clinics.map(clinic =>
@@ -60,29 +69,29 @@ class DoctorProfile extends React.Component {
                     </Grid>
 
                     <Grid style={{ paddingRight: "4%" }} item sm={5}>
-                        <Paper elevation={5} style={{ 'paddingRight': "4%", 'paddingLeft': "1%", opacity: "0.9" , background: "linear-gradient(to right,#90caf9, #1e88e5, #64b5f6)" }}>
-                           <div>
-                           <div>
-                                <div style={{ 'marginTop': "3%", 'marginRight': "15%" }}>
-                                    <img src={this.props.doctor.picture}
-                                        alt={this.props.doctor.user.first_name + " " + this.props.doctor.user.last_name}
-                                        style=
-                                        {{
-                                            'alignSelf': "right",
-                                            'marginLeft': "90%",
-                                            width: "135px",
-                                            height: "135px",
-                                            'borderRadius': "50%",
+                        <Paper elevation={5} style={{ 'paddingRight': "4%", 'paddingLeft': "1%", opacity: "0.9", background: "linear-gradient(to right,#90caf9, #1e88e5, #64b5f6)" }}>
+                            <div>
+                                <div>
+                                    <div style={{ 'marginTop': "3%", 'marginRight': "15%" }}>
+                                        <img src={this.props.doctor.picture}
+                                            alt={this.props.doctor.user.first_name + " " + this.props.doctor.user.last_name}
+                                            style=
+                                            {{
+                                                'alignSelf': "right",
+                                                'marginLeft': "90%",
+                                                width: "135px",
+                                                height: "135px",
+                                                'borderRadius': "50%",
 
-                                        }} />
+                                            }} />
                                     </div>
-                               </div>
+                                </div>
                                 <div style={{ 'textAlign': "right", 'marginLeft': "30%" }}>
                                     <div style={{ 'paddingTop': "2%" }}>
                                         <p>دکتر {this.props.doctor.user.first_name + " " + this.props.doctor.user.last_name}</p>
                                     </div>
                                     <div>
-                                         <Rate Rate={this.props.doctor.rate} /> :
+                                        <Rate Rate={this.props.doctor.rate} /> :
                                     </div>
                                     <div style={{ 'paddingTop': "2%" }}>
                                         <p>درباره پزشک</p>
@@ -94,12 +103,21 @@ class DoctorProfile extends React.Component {
                                         <p style={{ color: "grey" }}>{this.props.doctor.expertise}</p>
                                     </div>
                                 </div>
-                           </div>
+                            </div>
+                            <Button variant="contained"  style={{ 'marginTop': "10%" }}
+                                onClick={this.handleClickOnMap}> مشاهده نقشه</Button>
+
                             <br />
+                            <br/>
 
                         </Paper>
+                        
+                     
+
+
                     </Grid>
-                </Grid>
+            </Grid>
+            <Chat />
             </div>
 
         )
